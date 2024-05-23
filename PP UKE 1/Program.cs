ac3d713131e75edec315305751b2d7b7cd492ece
EvenPB <157693875+EvenPB@users.noreply.github.com>
2024-05-23 15:41:19 +0200

    Update 2?

--- PP UKE 1/Program.cs
+++ PP UKE 1/Program.cs
@@ -8,24 +8,56 @@ namespace PP_UKE_1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var D1 = new Deltaker
-            {
-                Name = "Kalle",
-                Age = 22,
-                Haircolor = "Dark blonde",
-                Hobby = "Fishing"
-            };
-            
-            
-            D1.PrintDeltaker();
 
-            var D2 = new Deltaker("Bjarne", 48, "bald", "mechanic");
-           
-            D2.PrintDeltaker();
+            CheckInput();
+
+
+
+
         }
-        
+
+
+        static void CheckInput()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Choice 1 Kalle");
+                Console.WriteLine("Choice 2 Bjarne");
+                Console.WriteLine("Choice 3 Nils");
+
+                string userChoice = Console.ReadLine();
+
+                var D1 = new Deltaker("Kalle", 22, "Dark Blond", "Fishing");
+
+                var D2 = new Deltaker("Bjarne", 48, "Bald", "mechanic");
+
+                var D3 = new Deltaker("Nils", 69, "Gray", "Shooting");
+
+                switch (userChoice)
+                {
+                    case "1":
+                        D1.PrintDeltaker();
+                        break;
+
+                    case "2":
+                        D2.PrintDeltaker();
+                        break;
+
+                    case "3":
+                        D3.PrintDeltaker();
+                        break;
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("unassigned choice");
+                        break;
+                }
+            }
+        }
     }
 
 }
