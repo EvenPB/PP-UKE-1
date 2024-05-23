7cdf51f7ed16b6d038e0367884737fe6f7b0b286
EvenPB <157693875+EvenPB@users.noreply.github.com>
2024-05-23 13:20:50 +0200

    Add project files.

---
+++ PP UKE 1/Deltaker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_UKE_1
+{
+    public class Deltaker
+    {
+        public string Name  { get; set; }
+
+        public int Age  { get; set; } 
+        public string Haircolor { get; set; }
+        public string Hobby { get; set; }
+
+        public Deltaker(string name, int age, string haircolor, string hobby)
+        {
+            Name = name;
+            Age = age;
+            Haircolor = haircolor;
+            Hobby = hobby;
+
+        }
+
+        public Deltaker()
+        {
+            
+        }
+        public void PrintDeltaker()
+        {
+            Console.WriteLine($"Hey, im {Name}! I'm {Age} Years old and my favorite hobby is {Hobby}");
+        }
+    }
+    
+
+}
---
+++ PP UKE 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_UKE_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var D1 = new Deltaker
+            {
+                Name = "Kalle",
+                Age = 22,
+                Haircolor = "Dark blonde",
+                Hobby = "Fishing"
+            };
+            
+            
+            D1.PrintDeltaker();
+
+            var D2 = new Deltaker("Bjarne", 48, "bald", "mechanic");
+           
+            D2.PrintDeltaker();
+        }
+        
+    }
+
+}
